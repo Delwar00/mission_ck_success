9cf1a4b71f90d7ac35a7b905936d63f99c5dce0a
Delwar00 <44938455+Delwar00@users.noreply.github.com>
2020-04-03 11:38:01 +0600

    Edit db

--- SQL/4-march/hrm.sql
+++ SQL/4-march/hrm.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.0.1
+-- version 4.8.3
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Mar 04, 2020 at 05:59 PM
--- Server version: 10.4.11-MariaDB
--- PHP Version: 7.4.3
+-- Generation Time: Apr 03, 2020 at 07:34 AM
+-- Server version: 10.1.35-MariaDB
+-- PHP Version: 7.2.9
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
@@ -25,12 +25,61 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `blogs`
+--
+
+CREATE TABLE `blogs` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `blog_posts`
+--
+
+CREATE TABLE `blog_posts` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `details` longtext COLLATE utf8mb4_unicode_ci NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data for table `blog_posts`
+--
+
+INSERT INTO `blog_posts` (`id`, `details`, `created_at`, `updated_at`) VALUES
+(1, 'sCDS', NULL, NULL),
+(2, '<p>fdsgfdsfds</p>', NULL, NULL),
+(3, '<p>dsvds</p>', NULL, NULL),
+(4, '<p>dfsadsa1</p>', NULL, NULL),
+(5, '<p>dvzczx</p>', NULL, NULL),
+(6, '<p>fddsdfs</p>', NULL, NULL),
+(7, '<p>gfsdffds</p>', NULL, NULL),
+(8, '<p><u>dgfdsggfds</u></p>', NULL, NULL),
+(9, '<p>dsaffsfda</p>', NULL, NULL),
+(10, '<p><strong>dsaffsfda</strong></p>', NULL, NULL),
+(11, '<p><em>dsaffsfda</em></p>', NULL, NULL),
+(12, '<p><u>dfsdafdsadsafcdsa</u></p>', NULL, '2020-04-02 21:45:14'),
+(13, '<p><em><strong>df</strong></em></p>', NULL, NULL),
+(14, '<p><strong><em>Test1</em></strong></p>', NULL, '2020-04-02 21:46:17'),
+(15, '<p><img alt=\"\" src=\"http://localhost:8000/storage/uploads/Screenshot (11)_1585890019.png\" />dscdafdasdsa</p>', NULL, NULL),
+(16, '<p><strong><img alt=\"\" src=\"http://localhost:8000/storage/uploads/Screenshot (11)_1585890557.png\" style=\"height:768px; width:1366px\" />Lorem Ipsum</strong>&nbsp;is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry&#39;s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of&nbsp;</p>', NULL, '2020-04-02 23:09:23'),
+(17, '<p><img alt=\"\" src=\"http://localhost:8000/storage/uploads/Screenshot (11)_1585890884.png\" style=\"height:56px; width:100px\" /></p>\r\n\r\n<p><strong>Lorem Ipsum</strong>&nbsp;is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry&#39;s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of&nbsp;</p>\r\n\r\n<p><img alt=\"\" src=\"http://localhost:8000/storage/uploads/Screenshot (3)_1585890935.png\" style=\"height:93px; width:166px\" /></p>\r\n\r\n<p><strong>Lorem Ipsum</strong>&nbsp;is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry&#39;s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of&nbsp;</p>', NULL, NULL),
+(18, '<p><img alt=\"\" src=\"http://localhost:8000/storage/uploads/Screenshot (3)_1585891064.png\" style=\"height:93px; width:165px\" /></p>\r\n\r\n<p><strong>Lorem Ipsum</strong>&nbsp;is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry&#39;s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of&nbsp;</p>\r\n\r\n<p><img alt=\"\" src=\"http://localhost:8000/storage/uploads/Screenshot (16)_1585891212.png\" style=\"height:60px; width:106px\" /></p>\r\n\r\n<p><strong>Lorem Ipsum</strong>&nbsp;is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry&#39;s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of&nbsp;</p>', NULL, '2020-04-02 23:20:22');
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `departments`
 --
 
 CREATE TABLE `departments` (
   `id` bigint(20) UNSIGNED NOT NULL,
-  `department_name` int(11) NOT NULL,
+  `department_name` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL,
   `status` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT '1',
   `created_at` timestamp NULL DEFAULT NULL,
   `updated_at` timestamp NULL DEFAULT NULL
@@ -80,7 +129,7 @@ CREATE TABLE `failed_jobs` (
   `queue` text COLLATE utf8mb4_unicode_ci NOT NULL,
   `payload` longtext COLLATE utf8mb4_unicode_ci NOT NULL,
   `exception` longtext COLLATE utf8mb4_unicode_ci NOT NULL,
-  `failed_at` timestamp NOT NULL DEFAULT current_timestamp()
+  `failed_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 -- --------------------------------------------------------
@@ -100,14 +149,16 @@ CREATE TABLE `migrations` (
 --
 
 INSERT INTO `migrations` (`id`, `migration`, `batch`) VALUES
-(1, '2014_10_12_000000_create_users_table', 1),
-(2, '2014_10_12_100000_create_password_resets_table', 1),
-(3, '2019_08_19_000000_create_failed_jobs_table', 1),
-(4, '2020_03_04_155905_create_roles_table', 1),
-(5, '2020_03_04_162714_create_employees_table', 2),
-(6, '2020_03_04_162738_create_tracking_hours_table', 2),
-(7, '2020_03_04_165151_create_departments_table', 2),
-(8, '2020_03_04_165205_create_designations_table', 2);
+(19, '2014_10_12_000000_create_users_table', 1),
+(20, '2014_10_12_100000_create_password_resets_table', 1),
+(21, '2019_08_19_000000_create_failed_jobs_table', 1),
+(22, '2020_03_04_155905_create_roles_table', 1),
+(23, '2020_03_04_162714_create_employees_table', 1),
+(24, '2020_03_04_162738_create_tracking_hours_table', 1),
+(25, '2020_03_04_165151_create_departments_table', 1),
+(26, '2020_03_04_165205_create_designations_table', 1),
+(30, '2020_04_02_174036_create_blogs_table', 2),
+(31, '2020_04_03_015132_create_blog_posts_table', 3);
 
 -- --------------------------------------------------------
 
@@ -131,8 +182,8 @@ CREATE TABLE `roles` (
   `id` bigint(20) UNSIGNED NOT NULL,
   `slug` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL,
   `name` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `note` text COLLATE utf8mb4_unicode_ci DEFAULT NULL,
-  `status` tinyint(4) NOT NULL DEFAULT 1,
+  `note` text COLLATE utf8mb4_unicode_ci,
+  `status` tinyint(4) NOT NULL DEFAULT '1',
   `created_at` timestamp NULL DEFAULT NULL,
   `updated_at` timestamp NULL DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
@@ -187,13 +238,25 @@ CREATE TABLE `users` (
 --
 
 INSERT INTO `users` (`id`, `role_id`, `email`, `email_verified_at`, `status`, `password`, `remember_token`, `created_at`, `updated_at`) VALUES
-(1, 1, 'superadmin@gmail.com', NULL, '1', '$2y$10$VAYRH9qLJ2vjtAVhRSJUx./3LzjZd6cw1sg.WR8NX7gYDoaHt09Lm', NULL, NULL, NULL),
-(2, 2, 'admin@gmail.com', NULL, '1', '$2y$10$F88PNCT2LBfjz9COziyF.uSC5zNyL./NTf86nBhP9Fa2aY1zIp.dW', NULL, NULL, NULL),
-(3, 3, 'employee@gmail.com', NULL, '1', '$2y$10$xHuxkiZhqP6x4UUy4Hn7duegkzO6y/gGxd7imtib.ld7Boyp8L2lO', NULL, NULL, NULL);
+(1, 1, 'superadmin@gmail.com', NULL, '1', '$2y$10$i5YYIScgEPJZK9/97tBui./ImyhJGHoHb8v1NHFFfo89UpcWhUHkC', NULL, NULL, NULL),
+(2, 2, 'admin@gmail.com', NULL, '1', '$2y$10$XdgRA.bBp0Z3at4NV2Y6KOHymQUN/SABPE8XuFrwV9MnVxk8qZ.SW', NULL, NULL, NULL),
+(3, 3, 'employee@gmail.com', NULL, '1', '$2y$10$.5Hhg7AOSSMRotVJV6rfuuGzc/l0ntSpc4lMNBqXZJjjV7xm2xjZS', NULL, NULL, NULL);
 
 --
 -- Indexes for dumped tables
 --
+
+--
+-- Indexes for table `blogs`
+--
+ALTER TABLE `blogs`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `blog_posts`
+--
+ALTER TABLE `blog_posts`
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Indexes for table `departments`
@@ -255,6 +318,18 @@ ALTER TABLE `users`
 --
 
 --
+-- AUTO_INCREMENT for table `blogs`
+--
+ALTER TABLE `blogs`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `blog_posts`
+--
+ALTER TABLE `blog_posts`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=19;
+
+--
 -- AUTO_INCREMENT for table `departments`
 --
 ALTER TABLE `departments`
@@ -282,7 +357,7 @@ ALTER TABLE `failed_jobs`
 -- AUTO_INCREMENT for table `migrations`
 --
 ALTER TABLE `migrations`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=32;
 
 --
 -- AUTO_INCREMENT for table `roles`
